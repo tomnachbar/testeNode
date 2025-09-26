ae31a86a5a839a28a91a4f507d02929cdf47dc88
Eliton Mauro Nachbar <140862399+tomnachbar@users.noreply.github.com>
2025-09-26 15:24:01 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rota para a home (index.html)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
